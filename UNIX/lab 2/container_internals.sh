2f5100137cfb0e8ba5142bc6c13011b1d3c7134c
JohnyAti <168288758+JohnyAti@users.noreply.github.com>
2025-12-04 16:09:03 +0700

    Add files via upload

--- UNIX/lab 2/container_internals.sh
+++ UNIX/lab 2/container_internals.sh
@@ -1,38 +1,38 @@
 #!/bin/sh
 container_code=$HOSTNAME
 counter=1
-shared_dir=/app/shared
+shared_dir=/shared_volume
 mkdir -p $shared_dir
 
 lock_file=$shared_dir/.lockfile
 touch $lock_file
 
-exec 9>"$lock_file" # Ставим в соответствие файлу lockfile дескриптор 9
+exec 9>"$lock_file"
 
 while :; do
-	created=""
-	flock -x 9
-	i=1
-	while [ $i -le 999 ]; do
-		filename=$(printf %03d $i)
-		path=$shared_dir/$filename
-		if [ ! -e "$path" ]; then
-			counter=$((counter+1))
-            touch $path
-			echo "$container_code $counter" > "$path"
-			created="$filename"
-			break
-		fi
-		i=$((i+1))
-	done
-	
-	flock -u 9
-	
-	if [ -n "$created" ]; then
-		sleep 1
-		rm "$shared_dir/$created"
-		sleep 1
-	else
-		sleep 2
-	fi
+    created=""
+    
+    flock -x 9
+    for name in @21 @22 @23 @24 @25; do
+        path=$shared_dir/$name
+        if [ ! -e "$path" ]; then
+            counter=$((counter+1))
+            echo "$container_code $counter" > "$path"
+            created="$name"
+            break
+        fi
+    done
+    flock -u 9
+    
+    if [ -n "$created" ]; then
+        sleep 1
+        if [ -f "$shared_dir/$created" ]; then
+            if head -n 1 "$shared_dir/$created" 2>/dev/null | grep -q "^$container_code "; then
+                rm "$shared_dir/$created"
+            fi
+        fi
+        sleep 1
+    else
+        sleep 2
+    fi
 done
--- UNIX/lab 2/run.sh
+++ UNIX/lab 2/run.sh
@@ -2,8 +2,10 @@
 # Первый аргумент вызова - название docker volume
 # Второй аргумент - количество одновременно запущенных контейнеров
 
-vol_name=$1;  # Наименование тома
-container_num=$2; # Количество контейнеров
+vol_name=$1
+container_num=$2
+
+docker volume create $vol_name 2>/dev/null || true
 
 exit_handler()
 {
@@ -12,26 +14,27 @@ exit_handler()
     trap - EXIT INT HUP QUIT TERM
     i=1
     while [ $i -le $container_num ]; do
-        docker stop "container_$i"
+        docker stop "container_$i" 2>/dev/null || true
         i=$((i+1))
     done
-    docker rmi lab2_unix
     exit $return_code
 }
 
 trap exit_handler INT HUP QUIT TERM EXIT
 
 docker build -t lab2_unix . || exit 1
-i=1;
+
+i=1
 while [ $i -le $container_num ]; do
-    docker run -d --rm --name "container_$i" --volume $vol_name:/app/shared lab2_unix || exit 2
-    i=$((i+1));
+    docker run -d --rm \
+        --name "container_$i" \
+        --volume $vol_name:/shared_volume \
+        lab2_unix || exit 2
+    i=$((i+1))
 done
 
-echo "Контенеры в работе"
+echo "Запущено $container_num контейнеров. Нажмите Ctrl+C для остановки."
 
-while :; do
-{
-    sleep 200
-}
+while true; do
+    sleep 3600
 done
